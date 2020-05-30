137dae5859a8dab2ffe7b76e4e579891d6682777
Герман Курочкин <ggkurochkin@edu.hse.ru>
2020-05-30 14:09:20 +0500

    Добавьте файлы проекта.

---
+++ ClassFrom10.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab14
+{
+    public interface IExecutable
+    {
+        void Show();
+        int ToWatt();
+
+    }
+    public class SortByPower : IComparer
+    {
+        int IComparer.Compare(object obj1, object obj2)
+        {
+            Engine eng1 = (Engine)obj1;
+            Engine eng2 = (Engine)obj2;
+            if (eng1.Power < eng2.Power) return -1;
+            else if (eng1.Power == eng2.Power) return 0;
+            else return 1;
+        }
+    }
+    public class Engine : IExecutable, IComparable, ICloneable
+    {
+        protected int power;
+        protected string name;
+        public int[] mas = new int[] { 1, 1, 1, 1, 1 };
+        Random rnd = new Random();
+        public int Power
+        {
+            get { return power; }
+            set
+            {
+                if (value < 0) power = 0;
+                else power = value;
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public Engine()
+        {
+            power = 0;
+            name = "-";
+        }
+        public Engine(string n, int f)
+        {
+            Name = n;
+            Power = f;
+        }
+        public override bool Equals(object e)
+        {
+            return Name == ((Engine)e).Name && Power == ((Engine)e).Power;
+        }
+        public virtual void Show()
+        {
+            Console.WriteLine($"Двигатель {name}; сила тяги - {power} лошадиных сил");
+        }
+        public override string ToString()
+        {
+            return $"Двигатель {name}; сила тяги - {power} лошадиных сил";
+        }
+        public void ShowNoVirt()
+        {
+            Console.WriteLine($"Двигатель {name}; сила тяги - {power} лошадиных сил");
+        }
+        public int ToWatt()
+        {
+            return (int)Math.Round(power * 735.5);
+        }
+        public int CompareTo(object obj)
+        {
+            Engine eng = (Engine)obj;
+            return string.Compare(name, eng.name);
+        }
+        public Engine ShallowCopy()
+        {
+            return (Engine)MemberwiseClone();
+        }
+        public object Clone()
+        {
+            return new Engine(name, power);
+        }
+        public Engine MakeRandom()
+        {
+            string[] names = new string[] {"SS","RR","TT","DD","FKIL","UYTG","MMNN","BHNG","LL-NN","VV-J","PP-I",
+            "CV","ND","KT","SL","XP","QL","SP","OU","WM","PP-N","CJ_NN","KKS","RT-3R","MK-4V","LLK-5","BB-6H"};
+            return new Engine(names[rnd.Next(27)], (rnd.Next(99) + 1) * 100);
+        }
+    }
+    public class InternalCEngine : Engine
+    {
+        Random rnd = new Random();
+        public override void Show()
+        {
+            Console.WriteLine($"Двигатель внутреннего сгорания {name}; сила тяги - {power} лошадиных сил");
+        }
+        public override string ToString()
+        {
+            return $"Двигатель внутреннего сгорания {name}; сила тяги - {power} лошадиных сил";
+        }
+        public new void ShowNoVirt()
+        {
+            Console.WriteLine($"Двигатель внутреннего сгорания {name}; сила тяги - {power} лошадиных сил");
+        }
+        public InternalCEngine() : base() { }
+        public InternalCEngine(string n, int f) : base(n, f) { }
+        public override bool Equals(object e)
+        {
+            return Name == ((InternalCEngine)e).Name && Power == ((InternalCEngine)e).Power;
+        }
+        public object Clone()
+        {
+            return new InternalCEngine(name, power);
+        }
+        public InternalCEngine MakeRandom()
+        {
+            string[] names = new string[] {"SS","RR","TT","DD","FKIL","UYTG","MMNN","BHNG","LL-NN","VV-J","PP-I",
+            "CV","ND","KT","SL","XP","QL","SP","OU","WM","PP-N","CJ_NN","KKS","RT-3R","MK-4V","LLK-5","BB-6H"};
+            return new InternalCEngine(names[rnd.Next(27)], (rnd.Next(99) + 1) * 100);
+        }
+        public Engine BaseEngine()
+        {
+            return new Engine(name, power);
+        }
+    }
+    public class Diesel : InternalCEngine
+    {
+        int numOfStroke;
+        Random rnd = new Random();
+        public int NumOfStroke
+        {
+            get { return numOfStroke; }
+            set
+            {
+                if (value == 4) numOfStroke = 4;
+                else numOfStroke = 2;
+            }
+        }
+        public override void Show()
+        {
+            Console.WriteLine($"Дизельный {numOfStroke}-тактный двигатель {name}; сила тяги - {power} лошадиных сил");
+        }
+        public override string ToString()
+        {
+            return $"Дизельный {numOfStroke}-тактный двигатель {name}; сила тяги - {power} лошадиных сил";
+        }
+        public new void ShowNoVirt()
+        {
+            Console.WriteLine($"Дизельный {numOfStroke}-тактный двигатель {name}; сила тяги - {power} лошадиных сил");
+        }
+        public Diesel() : base() { NumOfStroke = 2; }
+        public Diesel(string n, int f, int stroke) : base(n, f) { NumOfStroke = stroke; }
+        public override bool Equals(object e)
+        {
+            return Name == ((Diesel)e).Name && Power == ((Diesel)e).Power && NumOfStroke == ((Diesel)e).NumOfStroke;
+        }
+        public object Clone()
+        {
+            return new Diesel(name, power, numOfStroke);
+        }
+        public Diesel MakeRandom()
+        {
+            string[] names = new string[] {"SS","RR","TT","DD","FKIL","UYTG","MMNN","BHNG","LL-NN","VV-J","PP-I",
+            "CV","ND","KT","SL","XP","QL","SP","OU","WM","PP-N","CJ_NN","KKS","RT-3R","MK-4V","LLK-5","BB-6H"};
+            return new Diesel(names[rnd.Next(27)], (rnd.Next(99) + 1) * 100, rnd.Next(2) * 2 + 2);
+        }
+        public Engine BaseEngine()
+        {
+            return new Engine(name, power);
+        }
+    }
+    public class TurbojetEngine : InternalCEngine
+    {
+        bool afterburner;
+        Random rnd = new Random();
+        public bool Afterburner
+        {
+            get { return afterburner; }
+            set { afterburner = value; }
+        }
+        public override void Show()
+        {
+            string extra = "";
+            if (afterburner) extra = "; с форсажной камерой";
+            Console.WriteLine($"Турбореактивный двигатель {name}; сила тяги - {power} лошадиных сил" + extra);
+        }
+        public override string ToString()
+        {
+            string extra = "";
+            if (afterburner) extra = "; с форсажной камерой";
+            return $"Турбореактивный двигатель {name}; сила тяги - {power} лошадиных сил" + extra;
+        }
+        public new void ShowNoVirt()
+        {
+            string extra = "";
+            if (afterburner) extra = "; с форсажной камерой";
+            Console.WriteLine($"Турбореактивный двигатель {name}; сила тяги - {power} лошадиных сил" + extra);
+        }
+        public TurbojetEngine() : base() { afterburner = false; }
+        public TurbojetEngine(string n, int f, bool a) : base(n, f) { afterburner = a; }
+
+        public override bool Equals(object e)
+        {
+            return Name == ((TurbojetEngine)e).Name && Power == ((TurbojetEngine)e).Power && Afterburner == ((TurbojetEngine)e).Afterburner;
+        }
+        public object Clone()
+        {
+            return new TurbojetEngine(name, power, afterburner);
+        }
+        public TurbojetEngine MakeRandom()
+        {
+            string[] names = new string[] {"SS","RR","TT","DD","FKIL","UYTG","MMNN","BHNG","LL-NN","VV-J","PP-I",
+            "CV","ND","KT","SL","XP","QL","SP","OU","WM","PP-N","CJ_NN","KKS","RT-3R","MK-4V","LLK-5","BB-6H"};
+            return new TurbojetEngine(names[rnd.Next(27)], (rnd.Next(99) + 1) * 100, rnd.Next(2) == 1);
+        }
+        public Engine BaseEngine()
+        {
+            return new Engine(name, power);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace lab14
+{
+    class Program
+    {
+        static Engine eng1 = new Engine();
+        static InternalCEngine iEng1 = new InternalCEngine();
+        static Diesel dies1 = new Diesel();
+        static TurbojetEngine turb1 = new TurbojetEngine();
+        static int InputMenu(int maxNum)
+        {
+            int menu;
+            string input;
+            bool ok;
+            do
+            {
+                input = Console.ReadLine();
+                ok = int.TryParse(input, out menu);
+                if (!ok) Console.WriteLine("Некорректный ввод");
+                else if (menu < 0 || menu > maxNum) Console.WriteLine($"Некорректный ввод.Выберите вариант от 0 до {maxNum} из меню");
+            } while (!ok || menu < 0 || menu > maxNum);
+
+            return menu;
+        }
+
+        static int InputNum(int maxNum)
+        {
+            int num;
+            string input;
+            bool ok;
+            do
+            {
+                input = Console.ReadLine();
+                ok = int.TryParse(input, out num);
+                if (!ok) Console.WriteLine("Некорректный ввод");
+                else if (num < 0 || num > maxNum) Console.WriteLine($"Некорректный ввод. Введите число не больше {maxNum}");
+            } while (!ok || num < 0 || num > maxNum);
+
+            return num;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Collections col = new Collections(10);
+
+            col.Show();
+            Console.WriteLine("");
+
+            Sample(4000, col);      
+            Count(4000, col);      
+            Variety("CC", "TT", col); 
+            Grouping(6000, col);   
+            Aggregation(col);   
+        }
+
+        public static void Sample(int power, Collections col)
+        {
+            Console.WriteLine($"Выборка всех двигателей в мастерской, мощностью > {power}");
+
+            var linq = from eng in col.stackEng where eng.Power > power select eng;
+            foreach (Engine eng in linq)
+            {
+                eng.Show();
+            }
+
+            var methods = col.stackEng.Where(eng => eng.Power > power).Select(eng => eng);
+            Console.WriteLine();
+            foreach (Engine eng in methods)
+            {
+                eng.Show();
+            }
+
+            if (linq.Count() == methods.Count())
+                Console.WriteLine("Результаты совпадают");
+            Console.WriteLine();
+        }
+
+        public static void Count(int power, Collections col)
+        {
+            var linq = (from eng in col.listEng where eng.Power<power select eng).Count();
+
+            var methods = (col.listEng.Where(eng => eng.Power < power).Select(eng => eng)).Count();
+           
+
+            Console.WriteLine($"Кол-во двигателей в машине, мощностью меньше {power}: {linq};{methods}");
+            if (linq == methods)
+                Console.WriteLine("Результаты совпадают");
+            Console.WriteLine();
+        }
+
+        public static void Variety(string min, string max, Collections col)
+        {
+            Console.WriteLine($"Двигатели в машине, с именем от \"{min}\" и до \"{max}\" ");
+
+            var linq = (from eng in col.listEng where eng.Name.CompareTo(min)==1 select eng)
+                .Except(from eng in col.listEng where eng.Name.CompareTo(max) == 1 select eng);
+
+            var methods =
+                (col.listEng.Where(eng => eng.Name.CompareTo(min) == 1).Select(eng => eng)).Except
+                    (col.listEng.Where(eng => eng.Name.CompareTo(max) == 1).Select(eng => eng));
+
+            foreach (Engine eng in linq)
+            {
+                eng.Show();
+            }
+            Console.WriteLine();
+            foreach (Engine eng in methods)
+            {
+                eng.Show();
+            }
+
+            if (linq.Count() == methods.Count())
+                Console.WriteLine("Результаты совпадают");
+            Console.WriteLine();
+        }
+
+        public static void Aggregation(Collections col)
+        {
+            Engine SubAggregate(Engine max, Engine eng)
+            {
+                if (max.Power < eng.Power) return eng;
+                else return max;
+            }
+
+
+            var linq = (from eng in col.stackEng select eng.Power).Max();
+
+            var methods = (col.stackEng.Aggregate(SubAggregate)).Power;
+
+            Console.WriteLine($"Максимумальная мощность двигателя в мастерской: {linq}; {methods}");
+
+            if (linq == methods)
+                Console.WriteLine("Результаты LINQ запроса совпадают с результатами запроса с использованием методов расширения");
+
+            Console.WriteLine();
+        }
+
+        public static void Grouping(int num, Collections col)
+        {
+            Console.WriteLine($"Группировкка:Мощность двигателя в мастерской > {num}");
+
+            var linq = from eng in col.stackEng
+                       group eng by eng.Power > num;
+
+            var methods = col.stackEng.GroupBy(eng => eng.Power > num);
+
+            foreach (IGrouping<bool, Engine> g in linq)
+            {
+                Console.WriteLine(g.Key ? $"Мощность > {num}" : $"Мощность <= {num}");
+                foreach (var t in g)
+                    t.Show();
+                Console.WriteLine();
+            }
+            if (linq.Count() == methods.Count())
+                Console.WriteLine("Результаты совпадают");
+            Console.WriteLine();
+        }
+    
+
+    }
+
+    class Collections
+    {
+        public Stack<Engine> stackEng = new Stack<Engine>();
+        public List<Engine> listEng = new List<Engine>();
+
+
+        public int Size { get; set; }
+
+        public Collections(int size)
+        {
+            Random rand = new Random();
+            int menu;
+            Size = size;
+            InternalCEngine iEng = new InternalCEngine();
+            Diesel dies = new Diesel();
+            Engine eng = new Engine();
+            TurbojetEngine turb = new TurbojetEngine();
+            for (int i = 0; i < size; i++)
+            {
+                menu = rand.Next(4);
+
+                switch (menu)
+                {
+                    case 0:
+                        stackEng.Push(eng.MakeRandom());
+                        listEng.Add(eng.MakeRandom());
+                        break;
+                    case 1:
+                        stackEng.Push(dies.MakeRandom());
+                        listEng.Add(dies.MakeRandom());
+                        break;
+                    case 2:
+                        stackEng.Push(iEng.MakeRandom());
+                        listEng.Add(iEng.MakeRandom());
+                        break;
+                    case 3:
+                        stackEng.Push(turb.MakeRandom());
+                        listEng.Add(turb.MakeRandom());
+                        break;
+                }
+            
+
+            }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("stack-мастерская");
+            foreach (Engine elem in stackEng)
+            {
+                Console.WriteLine(elem.ToString());
+            }
+            Console.WriteLine("list-машины");
+            foreach (Engine elem in listEng)
+            {
+                Console.WriteLine(elem.ToString());
+            }
+        }
+
+        static int InputNum(int maxNum)
+        {
+            int num;
+            string input;
+            bool ok;
+            do
+            {
+                input = Console.ReadLine();
+                ok = int.TryParse(input, out num);
+                if (!ok) Console.WriteLine("Некорректный ввод");
+                else if (num < 0 || num > maxNum) Console.WriteLine($"Некорректный ввод. Введите число не больше {maxNum}");
+            } while (!ok || num < 0 || num > maxNum);
+
+            return num;
+
+        }
+        public void Add()
+        {
+            int menu;
+            Random rand = new Random();
+            InternalCEngine iEng = new InternalCEngine();
+            Diesel dies = new Diesel();
+            Engine eng = new Engine();
+            TurbojetEngine turb = new TurbojetEngine();
+            menu = rand.Next(4);
+
+            switch (menu)
+            {
+                case 0:
+                    stackEng.Push(eng.MakeRandom());
+                    listEng.Add(eng.MakeRandom());
+                    break;
+                case 1:
+                    stackEng.Push(dies.MakeRandom());
+                    listEng.Add(dies.MakeRandom());
+                    break;
+                case 2:
+                    stackEng.Push(iEng.MakeRandom());
+                    listEng.Add(iEng.MakeRandom());
+                    break;
+                case 3:
+                    stackEng.Push(turb.MakeRandom());
+                    listEng.Add(turb.MakeRandom());
+                    break;
+            }
+            Size++;
+        }
+        public void Delete()
+        {
+            stackEng.Pop();
+            listEng.RemoveAt(0);
+
+
+            Console.WriteLine("Элементы удален");
+            Size--;
+        }
+
+    }
+
+}
